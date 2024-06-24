e67e3936ddf3ea83511d37a4bd341820d42ed884
Gassgook <164035463+Gassgook@users.noreply.github.com>
2024-06-24 16:37:55 +0700

    Add files via upload

---
+++ Konversi.php
@@ -0,0 +1,14 @@
+
+
+
+<head>
+    <title>Konversi Nilai</title>
+</head>
+<body>
+    <form method="post">
+        <label for="nilai">Masukkan Nilai:</label>
+        <input type="number" id="nilai" name="nilai" required>
+        <button type="submit">Konversi</button>
+    </form>
+    <p>Nilai Anda: 100, Grade: A</p></body>
+
